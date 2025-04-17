cfaff46aba612698aa6faa2aad20754774fbe578
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 18:25:18 +0300

    urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import task_logs_page, trigger_task
+from .views import task_logs_page
 from . import views
 
 urlpatterns = [
     path('logs/', task_logs_page, name='task-logs-page'),
-    path('trigger-task/', trigger_task, name='trigger-task'),
+    # path('trigger-task/', trigger_task, name='trigger-task'),
     path("api/logs/", views.task_logs_api, name="task-logs-api"),
 ]
