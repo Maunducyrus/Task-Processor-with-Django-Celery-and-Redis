5626042cdccbe7925ce3eab83e198dda190b15d0
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 10:49:09 +0300

    admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TaskLog
 
-# Register your models here.
+@admin.register(TaskLog)
+class TaskLogAdmin(admin.ModelAdmin):
+    list_display = ('task_id', 'task_name', 'status', 'created_at')
+    search_fields = ('task_id', 'task_name')
+
