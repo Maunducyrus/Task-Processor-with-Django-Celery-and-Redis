f79a5ae2bfae3d11e3883cf45f5cd1229f783947
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 18:46:39 +0300

    tasks.py

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,25 +1,40 @@
+# from celery import shared_task
+# from .models import TaskLog
+
+# @shared_task(bind=True)
+# def sample_task(self, name):
+#     try:
+#         result = f"Task completed for {name}"
+
+#         TaskLog.objects.create(
+#             task_id=self.request.id,
+#             task_name=self.name,
+#             status="SUCCESS",
+#             result=result
+#         )
+
+#         return result
+
+#     except Exception as e:
+#         TaskLog.objects.create(
+#             task_id=self.request.id,
+#             task_name=self.name,
+#             status="FAILURE",
+#             result=str(e)
+#         )
+#         raise e
+
 from celery import shared_task
-from .models import TaskLog
+from tasks.models import TaskLog
 
 @shared_task(bind=True)
 def sample_task(self, name):
-    try:
-        result = f"Task completed for {name}"
-
-        TaskLog.objects.create(
-            task_id=self.request.id,
-            task_name=self.name,
-            status="SUCCESS",
-            result=result
-        )
-
-        return result
-
-    except Exception as e:
-        TaskLog.objects.create(
-            task_id=self.request.id,
-            task_name=self.name,
-            status="FAILURE",
-            result=str(e)
-        )
-        raise e
+    result = f"Task completed for {name}"
+    
+    # Update the result/status in DB
+    TaskLog.objects.filter(task_id=self.request.id).update(
+        status='SUCCESS',
+        result=result
+    )
+    
+    return result
