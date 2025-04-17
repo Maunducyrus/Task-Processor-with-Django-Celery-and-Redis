9ee1935f984ef654a291e65482e4d53836006519
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 18:01:28 +0300

    urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from .views import task_logs_page, trigger_task
 urlpatterns = [
     path('logs/', task_logs_page, name='task-logs-page'),
     path('trigger-task/', trigger_task, name='trigger-task'),
+    path("api/logs/", views.task_logs_api, name="task-logs-api"),
 ]
