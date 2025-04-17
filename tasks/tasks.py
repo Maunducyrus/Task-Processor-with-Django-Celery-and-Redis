999885d68a07e48e18764e9f661830b78ad6aba2
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 04:39:00 +0300

    debug

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,7 +1,6 @@
 from celery import shared_task
-import time
 
 @shared_task
-def sample_task(name="default"):
+def sample_task(name="Cyrus"):
     print(f"Processing task for {name}")
     return f"Hello, {name}! Task completed successfully."
