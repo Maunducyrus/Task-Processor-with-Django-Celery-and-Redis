4fcf781c2622fa4fd07555da4009dbac74bf4f65
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 18:55:21 +0300

    checking

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse
-from .models import TaskLog
 from django.shortcuts import render
+from .models import TaskLog
+from tasks.tasks import sample_task  # <-- celery task
+
+def task_logs_page(request):
+    return render(request, "tasklogs.html")
 
 def task_logs_api(request):
     logs = TaskLog.objects.order_by('-created_at')[:10]
@@ -16,5 +20,17 @@ def task_logs_api(request):
     ]
     return JsonResponse(data, safe=False)
 
-def task_logs_page(request):
-    return render(request, "tasklogs.html")
+def trigger_task(request):
+    name = request.GET.get('name', 'Anonymous')
+    task = sample_task.delay(name)
+
+    TaskLog.objects.create(
+        task_id=task.id,
+        task_name='sample_task',
+        status='PENDING'
+    )
+
+    return JsonResponse({
+        'message': 'Task has been triggered!',
+        'task_id': task.id
+    })
