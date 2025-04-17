1af39095b6db4b6b52bae267e40498ae176f624d
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 18:49:44 +0300

    views

--- tasks/views.py
+++ tasks/views.py
@@ -1,22 +1,20 @@
 from django.http import JsonResponse
 from .models import TaskLog
-
 from django.shortcuts import render
 
-def task_logs_page(request):
-    return render(request, "tasklogs.html")
-
-
 def task_logs_api(request):
-    logs = TaskLog.objects.all().order_by('-created_at')
+    logs = TaskLog.objects.order_by('-created_at')[:10]
     data = [
         {
-            "task_id": log.task_id,
-            "name": log.name,
-            "status": log.status,
-            "result": log.result,
-            "created_at": log.created_at.strftime('%Y-%m-%d %H:%M:%S')
+            'task_id': log.task_id,
+            'task_name': log.task_name,
+            'status': log.status,
+            'result': log.result,
+            'created_at': log.created_at.strftime('%Y-%m-%d %H:%M:%S')
         }
         for log in logs
     ]
     return JsonResponse(data, safe=False)
+
+def task_logs_page(request):
+    return render(request, "tasklogs.html")
