c8f5885b6cf902200957dd38662f611c9be4f242
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 10:45:50 +0300

    commit

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,7 +1,22 @@
-# from celery import shared_task
+from celery import shared_task
+from .models import TaskLog
 
-# @shared_task
-# def sample_task(name="Cyrus"):
-#     print(f"Processing task for {name}")
-#     return f"Hello, {name}! Task completed successfully."
+@shared_task(bind=True)
+def sample_task(self, name):
+    task_log, created = TaskLog.objects.get_or_create(
+        task_id=self.request.id,
+        defaults={"task_name": self.name, "status": "STARTED"}
+    )
 
+    try:
+        result = f"Task completed for {name}"
+        task_log.status = "SUCCESS"
+        task_log.result = result
+        task_log.save()
+        return result
+
+    except Exception as e:
+        task_log.status = "FAILURE"
+        task_log.result = str(e)
+        task_log.save()
+        raise
