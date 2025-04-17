40f5a56c71a31acc74813f0fada94f712884b2b9
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 13:42:10 +0300

    updates

---
+++ tasks/tasks.py
@@ -0,0 +1,25 @@
+from celery import shared_task
+from .models import TaskLog
+
+@shared_task(bind=True)
+def sample_task(self, name):
+    try:
+        result = f"Task completed for {name}"
+
+        TaskLog.objects.create(
+            task_id=self.request.id,
+            task_name=self.name,
+            status="SUCCESS",
+            result=result
+        )
+
+        return result
+
+    except Exception as e:
+        TaskLog.objects.create(
+            task_id=self.request.id,
+            task_name=self.name,
+            status="FAILURE",
+            result=str(e)
+        )
+        raise e
