c1e7d3fd6eb10b52f98a7a114fa7761defc73cec
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 04:10:33 +0300

    tasks urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import trigger_task
+from .views import trigger_task, check_task_status
 
 urlpatterns = [
     path('trigger-task/', trigger_task, name='trigger-task'),
+    path('task-status/<str:task_id>/', check_task_status, name='task-status'),
 ]
