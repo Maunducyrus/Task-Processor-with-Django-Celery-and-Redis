e10a119dd1355e3e3f57f929c00fba1f9c525523
Maunducyrus <maunducyrus123@gmail.com>
2025-04-17 10:42:04 +0300

    comment

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,6 +1,7 @@
-from celery import shared_task
+# from celery import shared_task
 
-@shared_task
-def sample_task(name="Cyrus"):
-    print(f"Processing task for {name}")
-    return f"Hello, {name}! Task completed successfully."
+# @shared_task
+# def sample_task(name="Cyrus"):
+#     print(f"Processing task for {name}")
+#     return f"Hello, {name}! Task completed successfully."
+
